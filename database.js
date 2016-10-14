382bddfe27b12221a30e7cf2f0fe3adfe65ee0be
TimVosch <timbox@live.nl>
2016-10-15 00:50:07 +0200

    Added sequelize encrypt option for azure

--- database.js
+++ database.js
@@ -3,17 +3,22 @@ var Sequelize = require('sequelize');
 var sequelize = new Sequelize(process.env.SQL_DATABASE, process.env.SQL_USER, process.env.SQL_PASS, {
     host: process.env.SQL_HOST || 'localhost',
     dialect: 'mssql',
+    dialectOptions: {
+        encrypt: true
+    },
+    port: 1433,
 
     pool: {
-        max: process.env.SQL_MAX_POOL_SIZE || 30,
+        max: process.env.SQL_MAX_POOL_SIZE || 5,
         min: 0,
         idle: 10000
     }
 });
 
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(function(err) {
-    console.log('Database connection has been established successfully.');
+    console.log('Connection has been established successfully.');
   })
   .catch(function (err) {
     console.log('Unable to connect to the database:', err);
