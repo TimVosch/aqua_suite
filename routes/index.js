a61a064b3d3a99099c3749b8753d89d5ac7abdab
TimVosch <timbox@live.nl>
2016-10-14 18:02:09 +0200

    Prove hybride api/view site

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var json = { title: 'Express' };
+
+  res.format({
+    html: function() {
+      res.render('index', json);
+    },
+
+    json: function() {
+      res.json(json);
+    }
+  });
 });
 
 module.exports = router;
