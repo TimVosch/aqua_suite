92b044014fdbcbe84f4d332f46aec9d3570ca29d
TimVosch <timbox@live.nl>
2016-10-17 12:17:28 +0200

    Create login route and generate JWT #6 #8

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,6 @@
+var Account = require('../models/account');
+var jwt = require('jsonwebtoken');
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,13 +8,32 @@ router.get('/', function (req, res, next) {
     var json = { pageName: 'login' };
     res.format({
         html: function() {
-            res.render('login', json);
+            return res.render('login', json);
         },
 
         json: function() {
-            res.json({error: true, message: "API Login not allowed!"});
+            return res.json({error: true, message: "POST for api login"});
         }
     });
+});
+
+router.post('/', function (req, res, next) {
+    if (!req.body.username || !req.body.password) {
+        return res.json({error: true, message: "Missing username or password"});
+    }
+    
+    Account.findOne({ where: { username: req.body.username } })
+        .then(function (account){
+            if (account.get('password') == req.body.password) {
+                var token = jwt.sign({
+                    username: account.username,
+                    firstname: account.firstname
+                }, process.env.SHARED_SECRET);
+                return res.json({ token });
+            } else {
+                return res.json({ error: true, message: "Login failed" });
+            }
+        });
 });
 
 module.exports = router;
