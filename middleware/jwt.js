793878d678da1c600e386a4a915a6baf0d59fc20
TimVosch <timbox@live.nl>
2016-10-18 11:40:03 +0200

    Fix wrong cookie name: token -> jwtoken

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -4,8 +4,8 @@ module.exports = jwt({
     getToken: function (req) {
         if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
             return req.headers.authorization.split(' ')[1];
-        } else if (req.cookies.token) {
-            return req.cookies.token;
+        } else if (req.cookies.jwtoken) {
+            return req.cookies.jwtoken;
         }
         return null;
     }
