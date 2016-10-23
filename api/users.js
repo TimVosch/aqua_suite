f2294ffe81dd9293a0b6d87fe6ccfcb9a8a64c1e
TimVosch <timbox@live.nl>
2016-10-23 12:30:20 +0200

    Separate API from route #17 #19

--- api/users.js
+++ api/users.js
@@ -5,133 +5,105 @@ var request = require('request');
 // Database models
 var user_model = require('../models/user');
 
-module.exports = {
+var users = {};
 
-    getUserProjects: function (username) {
-        return new Promise(function (resolve, reject) {
-            // Find user in database
-            user_model.findOne({
-                where: {
-                    username
+/**
+ * Retrieve all starred repositories for the user
+ */
+users.getUserProjects = function (username) {
+    return new Promise(function (resolve, reject) {
+        // Find user in database
+        user_model.findOne({
+            where: {
+                username
+            }
+        })
+        .then(function (user) {
+            var projects = [];
+            /**
+             * TO CHECK: can user be empty?
+             */
+            // Get starred repos
+            request(
+                    {
+                        url: 'https://api.github.com/users/' + user.githubname + '/starred',
+                        headers: {
+                            'Accept': 'application/vnd.github.v3+json',
+                            'user-agent': 'aqua_suite'
+                        },
+                        auth: {
+                            user: process.env.GITHUB_USERNAME,
+                            pass: process.env.GITHUB_PASSWORD
+                        }
+                    }, function (error, response, body) {
+                        // After receiving a response
+                        // Handle errors
+                        if (error){
+                            info('getUserProjects: githubapi returned an error: ', error);
+                            reject(error);
+                        }else if(response.statusCode >= 400) {
+                            info('getUserProjects: githubapi returned an error: ', body);
+                            reject(body);
+                        }
+                        // Extract projects data from repos
+                        var json = JSON.parse(body);
+                        for(var i=0; i < json.length; i++) {
+                            var _project = {
+                                name: json[i].name,
+                                fullname: json[i].full_name,
+                                description: json[i].description,
+                                htmlUrl: json[i].html_url,
+                                createdAt: json[i].created_at,
+                                updatedAt: json[i].updated_at,
+                                stargazersCount: json[i].stargazers_count,
+                                watchersCount: json[i].watchers_count,
+                                openIssuesCount: json[i].open_issues_count
+                            }
+                            // Calculate amount of weeks since sunday before creation of repo
+                            // Get created at date
+                            var date = new Date(_project.createdAt);
+                            // Get sunday
+                            date.setDate(date.getDate() - date.getDay());
+                            // Difference in milliseconds
+                            var date_difference = (new Date()).getTime() - date.getTime();
+                            // Divide milliseconds by milliseconds per week
+                            var weeks = Math.ceil(date_difference / (1000 * 60 * 60 * 24 * 7));
+                            _project.weeks = weeks;
+                            projects.push(_project);
+                        }
+                        resolve(projects);
+                    });
+        })
+        .catch(function (e) {
+            info(e);
+            reject(e);
+        })
+    });
+};
+
+/**
+ * Retrieve a single specified repository for the user
+ */
+users.getUserProject = function(username, repo_owner, repo_name) {
+    return new Promise(function (resolve, reject) {
+        // Check if the user is involved in the chosen project
+        users.getUserProjects(username)
+        .then(function (result) {
+            var _repo_url = (repo_owner + '/' + repo_name).toLowerCase();
+            for(var i=0; i < result.length; i++) {
+                // Check if repository name equals one of the involved ones.
+                if ((result[i].fullname).toLowerCase() == _repo_url) {
+                    _isInvolved = true;
+                    return resolve(result[i]);
                 }
-            })
-            .then(function (user) {
-                var projects = [];
-                /**
-                 * TO CHECK: can user be empty?
-                 */
-                // Get starred repos
-                request(
-                        {
-                            url: 'https://api.github.com/users/' + user.githubname + '/starred',
-                            headers: {
-                                'Accept': 'application/vnd.github.v3+json',
-                                'user-agent': 'aqua_suite'
-                            },
-                            auth: {
-                                user: process.env.GITHUB_USERNAME,
-                                pass: process.env.GITHUB_PASSWORD
-                            }
-                        }, function (error, response, body) {
-                            // After receiving a response
-                            // Handle errors
-                            if (error){
-                                reject(error);
-                            }else if(response.statusCode >= 400) {
-                                reject({ statusCode: response.statusCode, message: body });
-                            }
-                            // Extract projects data from repos
-                            var json = JSON.parse(body);
-                            for(var i=0; i < json.length; i++) {
-                                var _project = {
-                                    name: json[i].name,
-                                    fullname: json[i].full_name,
-                                    createdAt: json[i].created_at
-                                }
-                                // Calculate amount of weeks since sunday before creation of repo
-                                // Get created at date
-                                var date = new Date(_project.createdAt);
-                                // Get sunday
-                                date.setDate(date.getDate() - date.getDay());
-                                // Difference in milliseconds
-                                var date_difference = (new Date()).getTime() - date.getTime();
-                                // Divide milliseconds by milliseconds per week
-                                var weeks = Math.ceil(date_difference / (1000 * 60 * 60 * 24 * 7));
-                                _project.weeks = weeks;
-                                projects.push(_project);
-                            }
-                            resolve(projects);
-                        });
-            })
-            .catch(function (err) {
-                reject(err);
-            })
-        });
-    },
+            }
+            reject('User is not involved in this project');
+        })
+        .catch(function (e) {
+            info(e);
+            reject(e);
+        })
+    });
+};
 
-    /**
-     * Get all starred repos from a user
-     * PRABLAWM!: Test it, observe, aha problem!
-     */
-    getUserProjects2: function (username) {
-        // Find the requested user for his/hers github name
-        return new Promise(function (resolve, reject) {
-            user_model.find({
-                where: {
-                    username: username
-                }
-            })
-                .then(function (_user) {
-                    if (!_user) {
-                        info('getUserProjects: given user was not found');
-                        return reject({ message: 'User not found' });
-                    }
-                    // Retrieve subscribed repositories from user through github api
-                    request.get(
-                        {
-                            url: 'https://api.github.com/users/' + _user.githubname + '/starred',
-                            headers: {
-                                'Accept': 'application/vnd.github.v3+json',
-                                'user-agent': 'aqua_suite'
-                            },
-                            auth: {
-                                user: process.env.GITHUB_USERNAME,
-                                pass: process.env.GITHUB_PASSWORD
-                            }
-                        }, function (error, response, body) {
-                            if (error || response.statusCode >= 400) {
-                                info('getUserProjects: github api responded bad', body);
-                                return reject({ message: 'Github api reported an error' });
-                            }
-                            // Find all and calculate project age in weeks
-                            var gitapi_result = JSON.parse(body);
-                            var projects = [];
-                            for (var i = 0; i < gitapi_result.length; i++) {
-                                var _gitproject = gitapi_result[i];
-                                var _project = {
-                                    name: _gitproject.name,
-                                    fullname: _gitproject.full_name,
-                                    createdAt: _gitproject.created_at
-                                };
-                                // Calculate amount of weeks since sunday before creation of repo
-                                // Get created at date
-                                var date = new Date(_project.createdAt);
-                                // Get sunday
-                                date.setDate(date.getDate() - date.getDay());
-                                // Difference in milliseconds
-                                var date_difference = (new Date()).getTime() - date.getTime();
-                                // Divide milliseconds by milliseconds per week
-                                var weeks = Math.ceil(date_difference / (1000 * 60 * 60 * 24 * 7));
-                                _project.weeks = weeks;
-
-                                // Add this project to projects array
-                                projects.push(_project);
-                            }
-                            return resolve(projects);
-                        });
-                });
-        });
-    }
-
-
-}
+module.exports = users;
--- routes/users.js
+++ routes/users.js
@@ -11,59 +11,6 @@ var request = require('request');
 // Pre-defined functions
 
 /**
- * Get information about user' project
- */
-function getUserProject(username, repo_owner, repo_name) {
-    return new Promise(function (resolve, reject) {
-        users.getUserProjects(username)
-            .then(function (result) {
-                var _isInvolved = false;
-                // Check which is equal
-                for (var i=0; i < result.length; i++) {
-                    if ((result[i].fullname).toLowerCase() == (repo_owner + '/' + repo_name).toLowerCase()) {
-                        _isInvolved = true;
-                        // Request gitapi details for repo
-                        request.get({
-                            url: 'https://api.github.com/repos/' + repo_owner + '/' + repo_name,
-                            headers: {
-                                'Accept': 'application/vnd.github.v3+json',
-                                'user-agent': 'aqua_suite'
-                            },
-                            auth: {
-                                user: process.env.GITHUB_USERNAME,
-                                pass: process.env.GITHUB_PASSWORD
-                            }
-                        }, function (error, response, body) {
-                            // Error handling
-                            if (error || response.statusCode >= 400) {
-                                return resolve({ statusCode: 500, data: { error: true, message: 'Github api responded with an error' }});
-                            }
-                            var _gitapiResult = JSON.parse(body);
-                            // Only copy certain information
-                            var _data = {
-                                name: _gitapiResult.name,
-                                fullname: _gitapiResult.fullname,
-                                description: _gitapiResult.description,
-                                htmlUrl: _gitapiResult.html_url,
-                                createdAt: _gitapiResult.created_at,
-                                updatedAt: _gitapiResult.updated_at,
-                                stargazersCount: _gitapiResult.stargazers_count,
-                                watchersCount: _gitapiResult.watchers_count,
-                                openIssuesCount: _gitapiResult.open_issues_count
-                            };
-                            return resolve({ statusCode: 500, data: _data });
-                        });
-                    }
-                }
-                if (!_isInvolved) {
-                    // We haven't returned yet and thus the user is not involed
-                    return resolve({ statusCode:403,  data: { error: true, message: 'User is not involved in this project' } });
-                }
-            })
-        });
-}
-
-/**
  * Get specific commits from a repo
  */
 function getSpecificProjectCommits(username, repo_owner, repo_name, since, until) {
@@ -149,11 +96,15 @@ router.get('/:name/projects/:repo_owner/:repo_name', function (req, res, next) {
         username = req.user.username;
     }
     // return specific information
-    getUserProject(username, req.params.repo_owner, req.params.repo_name)
+    users.getUserProject(username, req.params.repo_owner, req.params.repo_name)
         .then(function (result) {
-            res.status(result.statusCode);
-            res.json(result.data);
-        });
+            res.json(result);
+        })
+        .catch(function (e) {
+            info(e);
+            res.status(400);
+            res.json({ error: true, message: e });
+        })
 });
 
 router.get('/:name/projects/:repo_owner/:repo_name/commits', function (req, res, next) {
