b075fbeb17b9e927c60202697f5948515a8b7844
TimVosch <timbox@live.nl>
2016-10-25 19:22:11 +0200

    Add log creation route, API #14

---
+++ api/logs.js
@@ -0,0 +1,43 @@
+var debug = require('debug');
+var info = debug('aqua:api_logs');
+var logModel = require('../models/log');
+var logInfoModel = require('../models/log_info');
+var utils = require('./utils');
+var users = require('./users');
+
+var logs = {};
+
+/**
+ * Create a new log and log_meta.
+ */
+logs.createLog = function(username, userId, repo_owner, repo_name, title, comments, week) {
+    return new Promise(function (resolve, reject) {
+        // First retrieve the project this log is for
+        users.getProject(username, repo_owner, repo_name)
+        .then(function (project) {
+            // Get project commits 
+            var dates = utils.weekToDates(project.createdAt, week);
+            return users.getProjectCommits(username, repo_owner, repo_name, dates.since, dates.until);
+        })
+        .then(function (commits) {
+            // build models
+            var log = logModel.create({
+                commitHistory: JSON.stringify(commits),
+                comments,
+                logInfo: {
+                    title,
+                    creatorId: userId
+                }
+            }, {
+                include: [logInfoModel]
+            });
+            resolve(log);
+        })
+        .catch(function(e) {
+            info(e);
+            reject(e);
+        })
+    });
+};
+
+module.exports = logs;
--- routes/logs.js
+++ routes/logs.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var user = require('../models/user');
 var log_info = require('../models/log_info');
 var log = require('../models/log');
+var logs = require('../api/logs');
 
 router.get('/create', function (req, res, next) {
     var json = { pageName: 'Create log', user: req.user };
@@ -20,6 +21,27 @@ router.get('/create', function (req, res, next) {
 router.get('/find', function (req, res, next) {
     return log_info.findAll().then(function (metas) {
         res.json(metas);
+    });
+});
+
+/**
+ * Creates a new log in the database
+ * body:
+ * {
+ *  title: 'STRING',
+ *  project: 'repoowner/reponame',
+ *  comments: 'TEXT',
+ *  week: 4
+ * }
+ */
+router.post('/create', function (req, res, next) {
+    logs.createLog(req.user.username, req.user.id, req.body.project.split('/')[0], req.body.project.split('/')[1], req.body.title, req.body.comments, req.body.week)
+    .then(function (result) {
+        res.json(result);
+    })
+    .catch(function (e) {
+        res.status(400);
+        res.json(e);
     });
 });
 
